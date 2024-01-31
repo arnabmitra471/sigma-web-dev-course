453f92da449179994e3545426702f9e2ace46193
Arnab Mitra <arnabmitra471@gmail.com>
2024-01-31 09:54:22 +0530

    Learning about var let and const in Javascript

---
+++ video54/script.js
@@ -0,0 +1,25 @@
+console.log("This is tutorial 55 of sigma web develoment course");
+
+let num1 = 54;
+let num2 = 67;
+let sum = num1 + num2;
+let c = "Arnab";
+console.log("The sum of "+num1+" and "+num2+" is "+sum)
+console.log(typeof num1,typeof num2,typeof c)
+
+var _name = "Shubham";
+console.log(_name)
+
+// Variables are case sensitive
+var $a = false;
+console.log($a)
+
+var $A = true;
+console.log($A)
+
+// var 55name = "Vrinda"; // This will give an error
+
+ /*const radius = 15;
+radius++; */  // Not Allowed when identifiers are declared with const keyword
+
+
