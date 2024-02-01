df572beb6f7e9d98691af89898d8af05aa37ccd0
Arnab Mitra <arnabmitra471@gmail.com>
2024-02-01 09:27:53 +0530

    Learnt about var let and const and all about variables in js

--- video55/script.js
+++ video55/script.js
@@ -1,21 +1,31 @@
 console.log("This is tutorial 55 of sigma web develoment course");
 
 let num1 = 54;
+let p = 23;
+p = p + 2
 let num2 = 67;
 let sum = num1 + num2;
 let c = "Arnab";
 console.log("The sum of "+num1+" and "+num2+" is "+sum)
 console.log(typeof num1,typeof num2,typeof c)
 
-var _name = "Shubham";
+let _name = "Shubham";
 console.log(_name)
 
 // Variables are case sensitive
-var $a = false;
+let $a = false;
 console.log($a)
 
-var $A = true;
+let $A = true;
 console.log($A)
+{
+    let p = "Shubham is a good boy";
+    console.log(p);
+    p = "A quick brown jumps over the lazy dog";
+    console.log(p)
+
+}
+console.log(p)
 
 // var 55name = "Vrinda"; // This will give an error
 
