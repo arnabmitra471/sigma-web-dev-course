8a3524ebb3af874ec5cace8bfa7e2908a7bd0f1d
Arnab Mitra <arnabmitra471@gmail.com>
2024-02-18 10:03:40 +0530

    Factorial using for loop in js

--- video65/factorial.js
+++ video65/factorial.js
@@ -1,10 +1,9 @@
 // Factorial using for loops
-const factorial = n=>{
+const factorial = n => {
     let fact = 1;
 
 
-    for(let i=1;i<=n;i++)
-    {
+    for (let i = 1; i <= n; i++) {
         fact *= i
     }
     return fact;
@@ -14,15 +13,5 @@ let n = parseInt(prompt("Enter a number to get its factorial: "))
 let res = factorial(n)
 console.log(`The factorial is ${res}`);
 
-// factorial using array.reduce() method
-let num = parseInt(prompt("Enter a number: "))
-let arr = [];
 
-for(i=1;i<=num;i++){
-    arr.push(i);
-}
-console.log(arr);
-let factorial_arr = arr.reduce((acc,num)=>{
-    return acc *= num;
-})
-console.log(`The factorial using reduce method is ${factorial_arr}`);
+
