e615e519bb7a89d58dec72a32df92cdcf0d67a7b
Arnab Mitra <arnabmitra471@gmail.com>
2024-02-18 10:34:18 +0530

    Fixed the error in calculating factorial with array.reduce() method

--- video65/factorial.js
+++ video65/factorial.js
@@ -13,5 +13,27 @@ let n = parseInt(prompt("Enter a number to get its factorial: "))
 let res = factorial(n)
 console.log(`The factorial is ${res}`);
 
+// factorial using array.reduce() method
 
+ function factorialWithReduce(number) {
+    if (number < 0) {
+        console.log("Factorial undefined for negative numbers");
+        return;
+    }
+    
+    else if(number === 0)
+    {
+        return 1
+    }
+    else
+    {
+        let arr = Array.from(Array(number + 1).keys());
+        let facReduce = arr.slice(1,).reduce((a, b) => a * b);
+        return facReduce;
+    }
+}
+
+let num = parseInt(prompt("Enter a number: "));
+let resultFactorial = factorialWithReduce(num)
+console.log(resultFactorial);
 
