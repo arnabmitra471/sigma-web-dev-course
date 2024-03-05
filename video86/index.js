5d15344752f2729379021dca360e7b31b99b4147
Arnab Mitra <arnabmitra471@gmail.com>
2024-03-05 10:12:17 +0530

    Learnt about CommonJS and ES6 modules and how to import and export them

---
+++ video86/index.js
@@ -0,0 +1,5 @@
+import  arnab from "./mymodule.js";
+import {num1,num3} from "./mymodule.js"
+
+console.log(num1,num3)
+console.log(arnab)
---
+++ video86/mymodule.js
@@ -0,0 +1,13 @@
+const obj = {
+    a: 56,
+    b: 23,
+    c: 87
+}
+
+let num1 = 16;
+let num2 = 45;
+let num3 = 90;
+export {num1}
+export {num2}
+export {num3}
+export  default obj;
