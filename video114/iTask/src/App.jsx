73bf6a50a407070abe4b7149a7dd8cb70179a9c5
Arnab Mitra <arnabmitra471@gmail.com>
2024-05-02 13:03:08 +0530

    toggling the showFinished state to show and hide finished todos

--- video114/iTask/src/App.jsx
+++ video114/iTask/src/App.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import { v4 as uuidv4 } from 'uuid';
+import { CiEdit } from "react-icons/ci";
+import { MdDelete } from "react-icons/md";
 
 function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([])
+  const [showFinished,setShowFinished] = useState(true)
 
   // Get all the todos from the local storage
   useEffect(() => {
 
     const todoString = localStorage.getItem("todos");
+    console.log(todoString)
     if (todoString) {
       let todos = JSON.parse(localStorage.getItem("todos"))
       setTodos(todos)
@@ -19,8 +23,6 @@ function App() {
   const saveToLS = () => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }
-
-  
 
   const handleAdd = () => {
     const newTodo = { id: uuidv4(), todo, isCompleted: false }
@@ -67,31 +69,35 @@ function App() {
     setTodos(newTodos)
     saveToLS()
   }
+  const toggleFinished = () => {
+    setShowFinished(!showFinished);
+  }
   return (
     <>
       <Navbar />
-      <div className="container mx-auto bg-violet-100 my-5 p-5 rounded min-h-[80vh]">
-
+      <div className="md:container md:mx-auto mx-3 bg-violet-100 my-5 p-5 rounded min-h-[80vh] md:w-1/2">
+        <h1 className="font-bold text-center text-xl underline decoration-purple-400">iTask - Manage your tasks at only one place</h1>
         <div className="addTodo">
           <h2 className="font-bold text-lg my-5">Add Todo</h2>
-          <input onChange={handleChange} value={todo} type="text" placeholder="Enter todo title" className="p-2 hover:bg-green-500 hover:text-white text-green-500 placeholder-green-500 hover:placeholder-white w-1/2 border-2 rounded-md border-purple-400 transition-colors" />
-          <button type="submit" className="m-4 px-2 py-1 bg-violet-600 hover:bg-violet-950 text-white rounded-md mx-6 text-lg" onClick={handleAdd}>Save</button>
+          <input onChange={handleChange} value={todo} type="text" placeholder="Enter todo title" className="p-2 hover:bg-green-500 hover:text-white text-green-500 placeholder-green-500 hover:placeholder-white w-1/2 border-2 rounded-full border-purple-400 transition-colors" />
+          <button type="submit" className="m-4 px-2 py-1 bg-violet-600 hover:bg-violet-950 text-white rounded-md mx-6 text-lg disabled:bg-violet-300" onClick={handleAdd} disabled={todo.length <=3}>Save</button>
         </div>
-        <h2 className="font-bold text-lg underline decoration-orange-600">Your Todos</h2>
+        <input type="checkbox" checked={showFinished} onChange={toggleFinished} className="mx-5"/>Show Finished
+        <h2 className="font-bold text-lg underline decoration-orange-600 my-4">Your Todos</h2>
 
-        <div className="todo flex w-1/2 justify-between flex-col">
+        <div className="todo flex md:w-1/2 justify-between flex-col w-full">
           {todos.length === 0 && <div className="m-5">No todos to display</div>}
           {todos.map(item => {
 
             return (
-              <div key={item.id} className="flex justify-between my-2 items-center">
+              (showFinished || !item.isCompleted) && <div key={item.id} className="flex justify-between my-2 items-center">
                 <div className="flex gap-5">
-                  <input onChange={handleCheckbox} type="checkbox" name={item.id} value={item.isCompleted} />
+                  <input onChange={handleCheckbox} type="checkbox" name={item.id} checked={item.isCompleted} />
                   <div className={`${item.isCompleted ? 'line-through' : ''} todo-text`}>{item.todo}</div>
                 </div>
                 <div className="buttons mx-4 flex h-full">
-                  <button type="button" className="m-4 px-2 py-1 bg-violet-600 hover:bg-violet-950 text-white rounded-md mx-1 text-lg" onClick={(e) => { handleEdit(e, item.id) }}>Edit</button>
-                  <button type="button" className="m-4 px-2 py-1 bg-violet-600 hover:bg-violet-950 text-white rounded-md mx-1 text-lg" onClick={(e) => { handleDelete(e, item.id) }}>Delete</button>
+                  <button type="button" className="m-4 px-2 py-1 bg-violet-600 hover:bg-violet-950 text-white rounded-md mx-1 text-lg" onClick={(e) => { handleEdit(e, item.id) }}><CiEdit /></button>
+                  <button type="button" className="m-4 px-2 py-1 bg-violet-600 hover:bg-violet-950 text-white rounded-md mx-1 text-lg" onClick={(e) => { handleDelete(e, item.id) }}><MdDelete /></button>
                 </div>
               </div>
             )
